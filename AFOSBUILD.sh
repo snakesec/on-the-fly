40b36fefeba60bf99abb14ca9655e870003deeef
Weidsom Nascimento <source@snakesecurity.org>
2025-05-23 12:55:35 -0700

    Update on-the-fly source code to HARDBASED 5.0

---
+++ AFOSBUILD.sh
@@ -0,0 +1,30 @@
+rm -rf /opt/ANDRAX/on-the-fly
+
+python3 -m venv /opt/ANDRAX/on-the-fly
+
+source /opt/ANDRAX/on-the-fly/bin/activate
+
+/opt/ANDRAX/on-the-fly/bin/pip3 install -r requirements.txt
+/opt/ANDRAX/on-the-fly/python3/bin/pip3 install netfilterqueue
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Pip install requirements... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf $(pwd) /opt/ANDRAX/on-the-fly/package
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf andraxbin/* /opt/ANDRAX/bin
